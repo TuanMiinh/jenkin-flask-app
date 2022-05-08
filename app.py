275be296d9b50ed4b7a9bf5b5e4f08c0b3a66e99
Tuan Minh <tuanminhbt72@gmail.com>
2022-05-08 21:26:54 +0700

    ADD JEKINFILE

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "HELLO WORLD!"
+    return "HELLO WORLD!!!!!!!!!"
 
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=6000)
-
 
 
